f0a70e425f407efeda5f9ea09ac5aca5f14b9fb3
luuufan <angelia.fan@gmail.com>
2018-08-13 14:18:17 -0700

    create session form components

---
+++ frontend/components/session/session_form_container.jsx
@@ -0,0 +1,21 @@
+import {connect} from 'react-redux';
+// import {createUser, createSession, clearErrors} from '../../actions/session';
+import SessionFrom from './session_form';
+
+const mapStateToProps = (state, ownProps) => {
+  let formType = ownProps.match.path === '/signup' ? 'signup' : 'login';
+  return ({
+    formType,
+    // errors: state.errors
+  });
+};
+
+// const mapDispatchToProps = (dispatch, ownProps) => {
+  // let action = ownProps.match.path === '/signup' ? createUser : createSession;
+  // return ({
+  //   action: user =>  dispatch(action(user)),
+  //   clearErrors: () => dispatch(clearErrors())
+//   });
+// };
+
+export default connect(mapStateToProps, null)(SessionFrom);
---
+++ frontend/session/session_form.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class SessionForm extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+
+		};
+	}
+
+	render(){
+		return (
+			<div>
+			</div>
+		);
+	}
+}
+
+export default SessionForm;
